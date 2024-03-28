72669d6fa652bebbd30dcc32b0ccc071218d84ce
herdiwongjava <eko.herdianto.job@gmail.com>
2024-03-28 16:09:17 +0800

    finish

--- js/scrip.js
+++ js/scrip.js
@@ -1,15 +1,27 @@
-var firstIndex=0;
-function automaticSlide(){
+function submitForm(){
+let form = document.forms['contact-form'];
+let nama = form['nama'].value;
+let email = form['email'].value;
+let slct = form['slct'].value;
+
+if(nama == '') alert ('nama masih kosong');
+if(email == '') alert ('email belum diisi');
+if(slct == '') alert ('data belum lengkap');
+}
+document.getElementById('send-button').addEventListener('click',() => submitForm())
+
+
+let indexSlide=0;
+function automaticSlide(n){
     setTimeout(automaticSlide,2000);
-    var pics;
-    const img=document.querySelectorAll('img');
-    for(pics=0; pics<img.length;pics++){
-        img[pics].style.display="none";
+    let listImage=document.getElementsByClassName('banner-img');
+    for(n=0; n<listImage.length;n++){
+        listImage[n].style.display="none";
     }
-    firstIndex++;
-    if(firstIndex > img.length){
-        firstIndex = 1;
+    indexSlide++;
+    if(indexSlide > listImage.length){
+        indexSlide = 1;
     }
-    img[firstIndex -1].style.display="block";
+    listImage[indexSlide -1].style.display="block";
 }
 automaticSlide();
